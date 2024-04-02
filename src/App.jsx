ca4d2b5f631042692ce8f6733184c2fbf86ff39d
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2024-04-02 19:31:49 +0530

    completed ThemeSwithcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 
-import './App.css'
+import { useEffect,useState } from 'react';
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 export default function App() {
+
+  const [themeMode, setThemeMode] = useState("light");
+  
+  const lightTheme = () =>{
+    setThemeMode("light")
+  }
+
+  const darkTheme = () =>{
+    setThemeMode("dark")
+  }
+
+  useEffect(() =>{
+    document.querySelector('html').classList.remove('light','dark');
+    document.querySelector('html').classList.add(themeMode);
+  },[themeMode])
+
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn/>
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card/>
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
+
   )
 }
